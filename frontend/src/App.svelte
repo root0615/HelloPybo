e865d85d36a0ad023c0a51f5dee64d85224d02a7
root_fit <120799289+root0615@users.noreply.github.com>
2025-12-23 15:23:14 +0900

    2-05-2 svelte router

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,21 +1,3 @@
-<script>
-  // question_list에 최초 빈 리스트를 초깃값으로 설정했다 만약 이렇게 하지 않을 경우 fetch 함수는 비동기 방식으로 실행되기 때문에
-  // 요청하는 중에 HTML 영역의 each 문이 실행되고 값이 없어 오류가 발생한다. 따라서 반복문을 사용할 경우 해당 값을 빈 리스트로 초기화하는 것이 좋다.
-  let question_list = []
-
-  
-  function get_question_list() {
-    fetch("http://127.0.0.1:8000/api/question/list").then((response) =>{
-      response.json().then((json) => {
-        question_list = json
-      })
-    })
-  }
-
-  get_question_list()
-</script>
-
-
 <!-- 
 // 반환 값의 JSON 형태를 그대로 출력하고 싶을 때 사용
 <pre>
@@ -43,8 +25,16 @@
 {객체.속성}
 -->
 
-<ul>
-  {#each question_list as question}
-    <li>{question.subject}</li>
-  {/each}
-</ul>
+<script>
+  // svelte-spa-router : 스벨트에서 라우터 기능을 담당하는 라이브러리
+  import Router from 'svelte-spa-router'
+  import Home from "./routes/Home.svelte"
+
+  const routes = {
+    '/': Home,
+  }
+</script>
+
+<Router {routes}/>
+
+
---
+++ frontend/src/routes/Home.svelte
@@ -0,0 +1,22 @@
+<script>
+  // question_list에 최초 빈 리스트를 초깃값으로 설정했다 만약 이렇게 하지 않을 경우 fetch 함수는 비동기 방식으로 실행되기 때문에
+  // 요청하는 중에 HTML 영역의 each 문이 실행되고 값이 없어 오류가 발생한다. 따라서 반복문을 사용할 경우 해당 값을 빈 리스트로 초기화하는 것이 좋다.
+  let question_list = []
+
+  
+  function get_question_list() {
+    fetch("http://127.0.0.1:8000/api/question/list").then((response) =>{
+      response.json().then((json) => {
+        question_list = json
+      })
+    })
+  }
+
+  get_question_list()
+</script>
+
+<ul>
+  {#each question_list as question}
+    <li>{question.subject}</li>
+  {/each}
+</ul>
