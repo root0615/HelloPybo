f1ac37ddd99cba29cc4d67a1535f06ae1bc0c043
root0615 <yungun4101@gmail.com>
2026-01-07 21:31:40 +0900

    3-06 qeustion.answers.length

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -79,6 +79,10 @@
           새로 고침을 하더라도 서버로 요청이 발생하지 않는다.
           -->
           <a use:link href="/detail/{question.id}">{question.subject}</a>
+          <!-- 답변 개수 -->
+          {#if question.answers.length > 0}
+          <span class="text-danger small mx-2">{question.answers.length}</span>
+          {/if}
         </td>
         <td>{moment(question.create_date).format("YYYY년 MM월 DD일 a hh:mm")}</td>
       </tr>
